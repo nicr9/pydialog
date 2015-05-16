90c90238e044fd76cf3c110637af1ab7d2dea509
Nic Roland <nicroland9@gmail.com>
2015-05-16 15:34:43 +0100

    Updated setup.py and added MIT license.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from distutils.core import setup
 
+VERSION = 'v0.1'
+
 setup(
     name='dialog',
-    version='v0.1',
+    version=VERSION,
     author='Nic Roland',
     author_email='nicroland9@gmail.com',
     packages=['dialog'],
     description="Command line dialogs made easy.",
     url = 'https://github.com/nicr9/dialog',
+    download_url = 'https://github.com/nicr9/dialog/tarball/%s' % VERSION,
+    license="MIT",
 )
